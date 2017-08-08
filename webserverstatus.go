d0547b6f75e69e5ba9096a3113589fb28df08941
Oleksiy Hryshchenko <greeschenko@gmail.com>
2017-08-08 01:44:21 +0000

    up

--- webserverstatus.go
+++ webserverstatus.go
@@ -59,40 +59,10 @@ func toJson(i interface{}) []byte {
 	return res
 }
 
-func main() {
-	getConfig()
-
-	myMux := http.NewServeMux()
-	myMux.HandleFunc("/api/main", srvMain)
-	myMux.HandleFunc("/api/status", srvStatus)
-	myMux.HandleFunc("/api/stat", srvStat)
-	myMux.HandleFunc("/api/sites", srvSites)
-	myMux.HandleFunc("/api/sites-status", srvSitesStatus)
-	myMux.HandleFunc("/api/sites-stats", srvSitesStats)
-	http.ListenAndServe(":1888", myMux)
-}
-
-func srvMain(w http.ResponseWriter, r *http.Request) {
-	data := Main{Conf.Server.Ip}
-	w.Write(toJson(data))
-}
-
-func srvStatus(w http.ResponseWriter, r *http.Request) {
-	var data Status
-	tmp := executeOne("bash scripts/pingstatus.sh " + Conf.Server.Ip)
-	if tmp == "fail" {
-		data = Status{"FAIL"}
-	} else {
-		data = Status{"OK"}
-	}
-	w.Write(toJson(data))
-}
-
-var ImgList = make(map[string][]int, len(Conf.Stats))
-
-func srvStat(w http.ResponseWriter, r *http.Request) {
+func genStatDataJson(s configread.Stats) []byte {
 	var statdata []Stat
-	for _, item := range Conf.Stats {
+
+	for _, item := range s {
 		var buffer bytes.Buffer
 		buffer.WriteString("/img/")
 		buffer.WriteString(item.Name)
@@ -127,7 +97,42 @@ func srvStat(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 
-	w.Write(toJson(statdata))
+	return toJson(statdata)
+}
+
+func main() {
+	getConfig()
+
+	myMux := http.NewServeMux()
+	myMux.HandleFunc("/api/main", srvMain)
+	myMux.HandleFunc("/api/status", srvStatus)
+	myMux.HandleFunc("/api/stat", srvStat)
+	myMux.HandleFunc("/api/sites", srvSites)
+	myMux.HandleFunc("/api/sites-status", srvSitesStatus)
+	myMux.HandleFunc("/api/sites-stats", srvSitesStats)
+	http.ListenAndServe(":1888", myMux)
+}
+
+func srvMain(w http.ResponseWriter, r *http.Request) {
+	data := Main{Conf.Server.Ip}
+	w.Write(toJson(data))
+}
+
+func srvStatus(w http.ResponseWriter, r *http.Request) {
+	var data Status
+	tmp := executeOne("bash scripts/pingstatus.sh " + Conf.Server.Ip)
+	if tmp == "fail" {
+		data = Status{"FAIL"}
+	} else {
+		data = Status{"OK"}
+	}
+	w.Write(toJson(data))
+}
+
+var ImgList = make(map[string][]int, len(Conf.Stats))
+
+func srvStat(w http.ResponseWriter, r *http.Request) {
+	w.Write(genStatDataJson(Conf.Stats))
 }
 
 func srvSites(w http.ResponseWriter, r *http.Request) {
@@ -166,46 +171,10 @@ func srvSitesStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func srvSitesStats(w http.ResponseWriter, r *http.Request) {
-	var statdata []Stat
 	index := r.URL.Query().Get("index")
 	id, err := strconv.Atoi(index)
 	if err != nil {
 		panic(err)
 	}
-	for _, item := range Conf.Sites[id].Stats {
-		var buffer bytes.Buffer
-		buffer.WriteString("/img/")
-		buffer.WriteString(item.Name)
-		buffer.WriteString(".png")
-
-		gval, err := strconv.ParseFloat(executeOne(item.Graphs[0].Command), 64)
-		if err != nil {
-			panic(err)
-		}
-
-		gmax, err := strconv.ParseFloat(executeOne(item.Graphs[0].Max), 64)
-		if err != nil {
-			panic(err)
-		}
-
-		if _, ok := ImgList[item.Name]; !ok {
-			ImgList[item.Name] = make([]int, 20)
-		}
-
-		ImgList[item.Name] = imggen.Gen(
-			ImgList[item.Name],
-			gval,
-			gmax,
-			"web"+buffer.String(),
-		)
-
-		statdata = append(statdata, Stat{
-			item.Name,
-			buffer.String() + "?time=" + time.Now().Format("20060102150405"),
-			executeOne(item.Condition),
-			executeOne(item.Status),
-		})
-	}
-
-	w.Write(toJson(statdata))
+	w.Write(genStatDataJson(Conf.Sites[id].Stats))
 }
--- webserverstatus_test.go
+++ webserverstatus_test.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestPropReadConfigFile(t *testing.T) {
-    getConfig()
-    if Conf.Server.Domen != "polonex.com.ua" {
-        t.Fatal("not prop server domen")
-    }
-    return
+	getConfig()
+	if Conf.Server.Domen != "polonex.com.ua" {
+		t.Fatal("not prop server domen")
+	}
+	return
 }
 
 func TestExecuteOne(t *testing.T) {
-    r := executeOne("printf OK")
-    if r != "OK" {
-        t.Fatal("execute commands from strings dont work")
-    }
-    return
+	r := executeOne("printf OK")
+	if r != "OK" {
+		t.Fatal("execute commands from strings dont work")
+	}
+	return
 }
 
 func TestToJson(t *testing.T) {
-    data := Status{"OK"}
-    r := toJson(data)
-    if len(r) == 0 {
-        t.Fatal("convert to json dont work")
-    }
-    return
+	data := Status{"OK"}
+	r := toJson(data)
+	if len(r) == 0 {
+		t.Fatal("convert to json dont work")
+	}
+	return
 }
