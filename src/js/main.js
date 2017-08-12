92890b3a3c51bf86c6e532cbcc365d15e6c59327
Oleksiy Hryshchenko <greeschenko@gmail.com>
2017-08-12 01:35:16 +0000

    up

--- src/js/main.js
+++ src/js/main.js
@@ -102,3 +102,45 @@ let MainPage = new Hata(
     },
     '/api/main'
 ).render();
+
+$('.mainhelp').bind('click', function() {
+    let ww = $(window).width();
+    let wh = $(window).height();
+    $('#helpwrap').attr('width', ww);
+    $('#helpwrap').attr('height', wh);
+    var c = document.getElementById("helpwrap");
+    var ctx = c.getContext("2d");
+    ctx.font = "20px Arial";
+    ctx.fillStyle = 'tomato';
+    ctx.fillText("site or server online status", ww / 2, 20);
+    ctx.fillText("stat name", 20, wh - 20);
+    ctx.fillText("stat graph", ww / 4, wh - 40);
+    ctx.fillText("stat status text", ww / 2, wh - 60);
+    ctx.fillText("stat status indicator", 3 * ww / 4, wh - 80);
+
+    let control = $('.mainpage_list').children().eq(0);
+    let p1 = control.children().eq(0).position();
+    let p2 = control.children().eq(1).position();
+    let p3 = control.children().eq(2).position();
+    let p4 = control.children().eq(3).position();
+
+    let s1 = $('#mainserverstatus').position();
+    let s2 = $('.mainpage_sites').children().eq(0).find('.mainpage_header_status').position();
+
+    ctx.moveTo(s1.left + 20, s1.top + 20);
+    ctx.lineTo(ww / 2 - 5, 15);
+    ctx.moveTo(s2.left + 20, s2.top + 20);
+    ctx.lineTo(ww / 2 - 5, 15);
+
+    ctx.moveTo(p1.left + 20, p1.top + 20);
+    ctx.lineTo(20, wh - 20 - 15);
+    ctx.moveTo(p2.left + 20, p2.top + 20);
+    ctx.lineTo(ww / 4, wh - 40 - 15);
+    ctx.moveTo(p3.left + 20, p3.top + 20);
+    ctx.lineTo(ww / 2, wh - 60 - 15);
+    ctx.moveTo(p4.left + 20, p4.top + 20);
+    ctx.lineTo(3 * ww / 4, wh - 80 - 15);
+    ctx.strokeStyle = 'tomato';
+    ctx.stroke();
+    $('#helpwrap').toggle();
+});
